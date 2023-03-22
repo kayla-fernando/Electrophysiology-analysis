c81acfb251ac56741cf550bdaff4bd0ed604c1a4
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2023-03-22 15:08:13 -0400

    Update access.m

--- access.m
+++ access.m
@@ -1,8 +1,10 @@
-function varargout = access(sweeps)
+function varargout = access(sweeps,search,Fs)
 % Edited by Kayla Fernando (5/4/22)
 % Calculate access resistance (Ra) for all determined range of sweeps 
 %   Inputs:
 %       sweeps = sweeps to analyze
+%       search = search window in ms, formatted [t1 t2] 
+%       Fs = sampling rate in Hz
 %   Outputs:
 %       [out] = the last column of allAccess, Ra in megaohms (MOhm)
 %       [out,table] = the last column of allAccess, Ra in megaohms (MOhm); table of calculations
@@ -10,7 +12,7 @@ function varargout = access(sweeps)
 sizeSweeps = size(sweeps);
 
     for ii = 1:sizeSweeps(2)
-        allAccess(ii,1) = min(sweeps(12800:12900,ii)); % current transient in pA
+        allAccess(ii,1) = min(sweeps(Fs*search(1):Fs*search(2),ii)); % current transient in pA
         allAccess(ii,2) = allAccess(ii,1)./1000000000000; % convert to A
         allAccess(ii,3) = -0.005 / allAccess(ii,2); % V = IR; solve for R given -0.005 V step; answer in ohms
         allAccess(ii,4) = allAccess(ii,3)./1000000; % access resistance in megaohms
