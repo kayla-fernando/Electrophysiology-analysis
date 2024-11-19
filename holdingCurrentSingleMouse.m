d47bc533db867fdfb1a5fc1e915aadc226ea9936
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2024-11-19 14:41:37 -0500

    Add files via upload

---
+++ holdingCurrentSingleMouse.m
@@ -0,0 +1,36 @@
+%% REMEMBER PATH CHANGES%%
+
+% Edited by Kayla Fernando (11/19/24)
+
+close all
+clear all
+clc 
+
+% Assumes using original recordings from Clampex
+folder = 'KF_241116'; % Naming conventions
+run = '2024_11_16_0009'; % Clampex ABF naming conventions
+basepath = 'Z:\\home\kayla\Electrophysiology\';
+mousepath = [folder '\' run '.abf'];
+[d,si,h] = abfload([basepath mousepath]); % Sampling at 50 kHz. d: columns number of samples in a single sweep by the number of sweeps in file; s: sampling interval in us; h: file information
+
+clc
+
+%% Calculate holding current for all EPSC sweeps and all IPSC sweeps %%
+
+% Select all EPSC sweeps
+allEPSC = d(:,13:27);
+% Select all IPSC sweeps
+allIPSC = d(:,1:10); 
+
+baseline_search = [0.001 0.100]; % search window in s
+Fs = 50000; % sampling rate in Hz
+
+for ii = 1:size(allEPSC,2)
+    baselineEPSC(ii) = mean(allEPSC((Fs*baseline_search(1)-49):(Fs*baseline_search(2)),ii));
+end
+for ii = 1:size(allIPSC,2)
+    baselineIPSC(ii) = mean(allIPSC((Fs*baseline_search(1)-49):(Fs*baseline_search(2)),ii));
+end
+
+avgIholdEPSC = mean(baselineEPSC)
+avgIholdIPSC = mean(baselineIPSC)
