eec2df7ac9c45130f48ec497b955519efc0fca30
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2024-11-25 14:14:28 -0500

    Update strontiumAnalysis.m

--- strontiumAnalysis.m
+++ strontiumAnalysis.m
@@ -5,12 +5,12 @@ close all
 clear all
 clc
 
-% Preprocessing: copy original .abf file, filter at 2 kHz 
+% Preprocessing: copy original .abf file, filter at 2 kHz
 
 % Load data
-folder = 'folder'; %Naming conventions
-run = 'run'; %Clampex ABF naming conventions
-basepath = 'Z:\\';
+folder = 'KF_241108_analyzed'; %Naming conventions
+run = '241108_0000 - Copy - analyzed - all traces'; %Clampex ABF naming conventions
+basepath = 'Z:\\home\kayla\Electrophysiology analysis\Strontium recordings\';
 mousepath = [folder '\' run '.abf'];
 [d,si,h] = abfload([basepath mousepath]); %Sampling at 50 kHz. d: columns number of samples in a single sweep by the number of sweeps in file; s: sampling interval in us; h: file information
 
@@ -20,7 +20,7 @@ clc
 sweep_number = [1 10];
 count = [];
 amplitudes_cell = {};
-search = [0.620 0.820]; % search window in s 
+search = [0.730 0.925]; % search window in s 
 Fs = 50000; % sampling rate in Hz
 
 % Savitzky-Golay filter parameters
@@ -32,6 +32,7 @@ thresholdFactor = 1.5;
 blanking_indices = 500;
 direction = 'down'; 
 
+forPlotting = cell(1,(sweep_number(2)-sweep_number(1)+1));
 for ii = sweep_number(1):sweep_number(2)
     
     % Sweep to analyze
@@ -40,16 +41,34 @@ for ii = sweep_number(1):sweep_number(2)
     % Filter and plot each sweep
     [filtered_signal_base,event_indices,ax1,ax2,ax3,ax4,threshold,gof] = plotFilteredSignalStrontium(single_sweep,run,sav_golay_order,sav_golay_bin_width,thresholdFactor,blanking_indices,direction);
     goodness_of_fit = gof.rsquare
+    if ismember(1,event_indices) == 1
+        event_indices = event_indices(2:end); %false positive
+    end
+    for n = 1:length(event_indices)
+        if event_indices(n)+450 < length(filtered_signal_base)
+            forPlotting{ii}(:,n) = filtered_signal_base(event_indices(n)-50:event_indices(n)+450); % 1 ms pre-dervative and 9 ms post-derivative
+        else
+            forPlotting{ii}(:,n) = vertcat(filtered_signal_base(event_indices(n)-50:length(filtered_signal_base)),...
+            NaN(501-length(filtered_signal_base(event_indices(n)-50:length(filtered_signal_base))),1));
+        end
+    end
 
     % Find amplitudes for all detected events for all sweeps (instead of using the amplitudes app) 
     amplitudes = eventAmplitudes(ii,filtered_signal_base,event_indices,direction);
     amplitudes_cell{ii} = amplitudes(:,ii);
     amplitudes_cell = amplitudes_cell(~cellfun('isempty',amplitudes_cell));
     [max_amp_events, idx] = max(cellfun('size',amplitudes_cell,1));
-    
+
 end
 
 amplitudes = vertcat(amplitudes_cell{:});
+forPlotting = horzcat(forPlotting{:});
+figure;
+for n = 1:size(forPlotting,2)
+    plot(forPlotting(:,n));
+    hold on
+end
+plot(mean(forPlotting,2,'omitnan'),'k','LineWidth',2)
 
 %% Make a histogram showing distribution of event amplitudes
 
