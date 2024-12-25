62be86908bcdc4d9d1ed687022a95a00e54ac7b8
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2024-12-25 14:43:51 -0500

    Update sortThy1.m

--- sortThy1.m
+++ sortThy1.m
@@ -6,8 +6,8 @@ clear all
 clc
 
 % Load data
-basepath = 'Z:\\home\kayla\Electrophysiology analysis\E-I Ratio in Thy1-ChR2\';
-mousepath = '241118 Coronal E-I for sorting.xlsx'; % Naming convention
+basepath = 'Z:\\';
+mousepath = 'filename.xlsx'; % Naming convention
 Thy1_pseudo_EPSC = readtable([basepath mousepath],'Sheet','Thy1 pseudotraining EPSC');
 Thy1_pseudo_IPSC = readtable([basepath mousepath],'Sheet','Thy1 pseudotraining IPSC');
 Thy1_opto_EPSC = readtable([basepath mousepath],'Sheet','Thy1 optotraining EPSC');
@@ -18,11 +18,11 @@ str = repmat("double", 1, 12); % for table preallocation
 Ra_threshold = 15.5; 
 Ihold_threshold = -550; 
 
-% Triaged Thy1 pseudotrained EPSC data
+% Thy1 pseudotrained EPSC data
 sort_Thy1_pseudo_EPSC = (Thy1_pseudo_EPSC.Ra_electrode_ <= Ra_threshold & Thy1_pseudo_EPSC.Ihold_electrode_ >= Ihold_threshold);
 T_Thy1_pseudo_EPSC = Thy1_pseudo_EPSC(sort_Thy1_pseudo_EPSC,:); clear sort_Thy1_pseudo_EPSC;
 
-% Triaged Thy1 pseudotrained IPSC data
+% Thy1 pseudotrained IPSC data
 T_Thy1_pseudo_IPSC = table('Size',[0 12],'VariableTypes',str);
 T_Thy1_pseudo_IPSC.Properties.VariableNames = ["pA_electrode_","pA_opto_","E_I_electrode_","E_I_opto_","ElecRun","OptoRun", ...
     "Ra_electrode_","Ri_electrode_","Ra_opto_","Ri_opto_","Ihold_electrode_","Ihold_opto_"];    
@@ -33,11 +33,11 @@ for ii = 1:size(T_Thy1_pseudo_EPSC,1)
     T_Thy1_pseudo_IPSC = [T_Thy1_pseudo_IPSC; sort_Thy1_pseudo_IPSC];
 end; clear sort_Thy1_pseudo_IPSC
 
-% Triaged Thy1 optotrained EPSC data
+% Thy1 optotrained EPSC data
 sort_Thy1_opto_EPSC = (Thy1_opto_EPSC.Ra_electrode_ <= Ra_threshold & Thy1_opto_EPSC.Ihold_electrode_ >= Ihold_threshold);
 T_Thy1_opto_EPSC = Thy1_opto_EPSC(sort_Thy1_opto_EPSC,:); clear sort_Thy1_opto_EPSC;
 
-% Triaged Thy1 optotrained IPSC data
+% Thy1 optotrained IPSC data
 T_Thy1_opto_IPSC = table('Size',[0 12],'VariableTypes',str);
 T_Thy1_opto_IPSC.Properties.VariableNames = ["pA_electrode_","pA_opto_","E_I_electrode_","E_I_opto_","ElecRun","OptoRun", ...
     "Ra_electrode_","Ri_electrode_","Ra_opto_","Ri_opto_","Ihold_electrode_","Ihold_opto_"];    
